288646f9efe8c32efd21a3eca9496eefd2bad088
BrainWosher <svprokopenko92@gmail.com>
2019-02-14 22:12:29 +0700

    сверстал блок с ходящим человеком
    пытаюсь заставить его разворачиваться и идти обратно

--- build/js/main.js
+++ build/js/main.js
@@ -12,11 +12,42 @@ $(document).ready(function() {
         spaceBetween: 30,
     });
 });
-// TweenMax.to('.events-skier', 5 ,{left:0});
-(function(){
-    TweenMax.set('.events-skier', {left: 1000, bottom: 46});
-    setInterval(function(){
-        TweenMax.set('.events-skier', {left: 1000, bottom: 46});
-        TweenMax.to('.events-skier', 5, {left: -1000, bottom: -46});
-    },5000);
+// let widthEventsBlock = document.body.clientWidth - 192;
+// (function() {
+//     TweenMax.set('.events-skier', { left: widthEventsBlock, bottom: 36 });
+//     setInterval(function() {
+//         TweenMax.set('.events-skier', { left: widthEventsBlock, bottom: 36 });
+//         TweenMax.to('.events-skier', 7, { left: -widthEventsBlock, bottom: -76 });
+//     }, 5000);
+// })();
+let widthEventsBlock = document.body.clientWidth - 192;
+(function() {
+    TweenMax.set('.events-skier', { left: widthEventsBlock, bottom: 36 });
+    TweenMax.to('.events-skier', 7, {
+        left: -widthEventsBlock,
+        repeat: -1,
+        repeatDelay: 1,
+        bottom: -76
+    });
+})();
+let widthWM = document.body.clientWidth - 154;
+let man = '.walk-man';
+const element = document.querySelector('.walk-man');
+const style = getComputedStyle(element);
+const left = style.left;
+console.log(left);
+(function() {
+    setInterval(function() {
+        TweenMax.to(man, 7, { left: widthWM });
+        if (left === widthWM) {
+            TweenMax.set(man, { transform: rotateY('180deg') });
+            TweenMax.to(man, 7, { left: -widthWM });
+        } else if (left === 0) {
+            TweenMax.set(man, { transform: rotateY('-180deg') });
+            TweenMax.to(man, 7, { left: widthWM });
+        } else {
+            console.log('что-то не так');
+        }
+        //     }, 5000);
+    })
 })();
